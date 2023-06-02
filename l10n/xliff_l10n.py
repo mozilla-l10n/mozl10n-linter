cbaa650532568b82324e57cd79f195d02e145ba6
Delphine Lebédel <lebedel.delphine@gmail.com>
2023-06-01 22:46:55 -0700

    Use same placeables structure for XLIFF exceptions
    Add exceptions for Turkish in Firefox for iOS

--- l10n/xliff_l10n.py
+++ l10n/xliff_l10n.py
@@ -111,7 +111,15 @@ def main():
                 # Check placeables
                 ref_matches = placeables_pattern.findall(ref_string)
                 if ref_matches:
-                    if string_id in exceptions.get("placeables", {}).get(locale, []):
+                    string_exceptions = exceptions.get("placeables", {}).get(
+                        "strings", []
+                    )
+                    locale_exceptions = (
+                        exceptions.get("placeables", {})
+                        .get("locales", {})
+                        .get(locale, [])
+                    )
+                    if string_id in locale_exceptions + string_exceptions:
                         continue
                     ref_matches.sort()
                     l10n_matches = placeables_pattern.findall(l10n_string)
