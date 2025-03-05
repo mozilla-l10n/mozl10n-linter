d0a0d69856f5847d273416203c8f56488242b0cb
Francesco Lodolo <flod@lodolo.net>
2025-03-05 12:04:39 +0100

    Improve mozl10n code, update requirements (#45)

--- l10n/android_l10n.py
+++ l10n/android_l10n.py
@@ -63,20 +63,23 @@ class StringExtraction:
         if not locales:
             print("No locales defined in the project configuration.")
 
+        all_files = [
+            (ref_path, tgt_path)
+            for (ref_path, tgt_path), _ in project_config_paths.all().items()
+        ]
         for locale in locales:
             print(f"Extracting strings for locale: {locale}.")
-            file_list = [
-                (
-                    os.path.abspath(
-                        tgt_path.format(android_locale=get_android_locale(locale))
-                    ),
-                    os.path.abspath(ref_path.format(android_locale=None)),
+            locale_files = [
+                (os.path.abspath(ref_path), os.path.abspath(tgt_path))
+                for (ref_path, raw_tgt_path) in all_files
+                if os.path.exists(
+                    tgt_path := project_config_paths.format_target_path(
+                        raw_tgt_path, locale
+                    )
                 )
-                for (ref_path, tgt_path), locales in project_config_paths.all().items()
-                if locale in locales
             ]
 
-            for l10n_file, ref_file in file_list:
+            for ref_file, l10n_file in locale_files:
                 # Ignore missing files in locale
                 if not os.path.exists(l10n_file):
                     # print(f"Ignored missing file for {locale}: {l10n_file}")
